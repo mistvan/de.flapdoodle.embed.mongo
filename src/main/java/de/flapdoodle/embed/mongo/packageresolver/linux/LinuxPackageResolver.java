a9bfc202f2e196c9ae564c4c65f7716a57f852b2
Michael Mosmann <michael@mosmann.de>
2021-10-29 00:39:53 +0200

    lic header

--- src/main/java/de/flapdoodle/embed/mongo/packageresolver/linux/LinuxPackageResolver.java
+++ src/main/java/de/flapdoodle/embed/mongo/packageresolver/linux/LinuxPackageResolver.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011
- * Michael Mosmann <michael@mosmann.de>
- * Martin Jöhren <m.joehren@googlemail.com>
- * <p>
+ *   Michael Mosmann <michael@mosmann.de>
+ *   Martin Jöhren <m.joehren@googlemail.com>
+ *
  * with contributions from
- * konstantin-ba@github,Archimedes Trajano	(trajano@github)
- * <p>
+ * 	konstantin-ba@github,Archimedes Trajano	(trajano@github)
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
