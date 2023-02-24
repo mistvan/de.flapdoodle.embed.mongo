dfe0de5998c6838d14221908bb8c6dcff0da65e3
Michael Mosmann <michael@mosmann.de>
2023-02-24 09:10:53 +0100

    add versions to test

--- src/test/java/de/flapdoodle/embed/mongo/scenario/AccessControlAndListCollectionsActionTest.java
+++ src/test/java/de/flapdoodle/embed/mongo/scenario/AccessControlAndListCollectionsActionTest.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (C) 2011
+ *   Michael Mosmann <michael@mosmann.de>
+ *   Martin Jöhren <m.joehren@googlemail.com>
+ *
+ * with contributions from
+ * 	konstantin-ba@github,Archimedes Trajano	(trajano@github)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.flapdoodle.embed.mongo.scenario;
 
 import com.mongodb.*;
@@ -15,11 +35,18 @@ import de.flapdoodle.reverse.transitions.Start;
 import org.bson.Document;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runners.Parameterized;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
@@ -36,13 +63,24 @@ public class AccessControlAndListCollectionsActionTest {
 	private final String rolename = "issue-442-role";
 	private final String action = "listCollections";
 
+	@TempDir
+	Path temp;
+
+	public static Stream<Arguments> versions() {
+		return Stream.of(
+			Arguments.of(Version.Main.V4_4),
+			Arguments.of(Version.Main.V5_0),
+			Arguments.of(Version.Main.V6_0)
+		);
+	}
+
 	/**
 	 * see https://github.com/flapdoodle-oss/de.flapdoodle.embed.mongo/issues/442#issuecomment-1438370904
 	 */
-	@Test
-	void createUserAndCollectionsWithoutAuthFirst(@TempDir Path temp) throws IOException {
+	@ParameterizedTest
+	@MethodSource(value = "versions")
+	void createUserAndCollectionsWithoutAuthFirst(Version.Main version) throws IOException {
 		int port = de.flapdoodle.net.Net.freeServerPort();
-		Version.Main version = Version.Main.V4_4;
 
 		Path databaseDir = temp.resolve("database");
 		Files.createDirectory(databaseDir);
@@ -133,10 +171,10 @@ public class AccessControlAndListCollectionsActionTest {
 		}
 	}
 
-	@Test
-	void createUserAndCollectionsWithAuthInSingleRun(@TempDir Path temp) throws IOException {
+	@ParameterizedTest
+	@MethodSource(value = "versions")
+	void createUserAndCollectionsWithAuthInSingleRun(Version.Main version) throws IOException {
 		int port = de.flapdoodle.net.Net.freeServerPort();
-		Version.Main version = Version.Main.V4_4;
 
 		Path databaseDir = temp.resolve("database");
 		Files.createDirectory(databaseDir);
