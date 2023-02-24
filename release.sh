8873a7a6e85d44dd0e93fdf43c27296f9918126c
Michael Mosmann <michael@mosmann.de>
2023-02-24 09:12:15 +0100

    no clean build with release script

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./mvnw clean install
+#./mvnw clean install
 ./mvnw release:clean
 ./mvnw release:prepare -DskipTests -Darguments=-DskipTests
 ./mvnw release:perform -DskipTests -Darguments=-DskipTests
