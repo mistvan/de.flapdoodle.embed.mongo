60b1950ae576d3113da3e9618ffe47805c8ae764
Michael Mosmann <michael@mosmann.de>
2023-07-31 20:10:35 +0200

    dep updates

--- src/main/java/de/flapdoodle/embed/mongo/distribution/Version.java
+++ src/main/java/de/flapdoodle/embed/mongo/distribution/Version.java
@@ -248,11 +248,13 @@ public enum Version implements IFeatureAwareVersion, HasMongotoolsPackage {
 	V6_0_4("6.0.4", MongotoolsVersion.Main.V100_6),
 	V6_0_5("6.0.5", MongotoolsVersion.Main.V100_7),
 	V6_0_6("6.0.6", MongotoolsVersion.Main.V100_7),
+	V6_0_8("6.0.8", MongotoolsVersion.Main.V100_7),
 
 	V6_3_1_DEV("6.3.1", MongotoolsVersion.Main.V100_7),
 
 	V7_0_0_RC1("7.0.0-rc1", MongotoolsVersion.Main.V100_7),
 	V7_0_0_RC2("7.0.0-rc2", MongotoolsVersion.Main.V100_7),
+	V7_0_0_RC8("7.0.0-rc8", MongotoolsVersion.Main.V100_7),
 
 	@Deprecated
   LATEST_NIGHTLY("latest"),
